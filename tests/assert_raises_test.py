1c2e551e112782239ba2975457bff8bb3dc928e6
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2011-03-08 22:18:00 +0100

    assert_raises completely tested

---
+++ tests/assert_raises_test.py
@@ -0,0 +1,96 @@
+# -*- coding: UTF-8 -*-
+#
+# The MIT License
+# 
+# Copyright (c) 2011 Felix Schwarz <felix.schwarz@oss.schwarz.eu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# I believe the license above is permissible enough so you can actually 
+# use/relicense the code in any other project. If you need relicensing under any
+# other free software license, I'm happy to do that.
+
+from unittest import TestCase
+
+from pythonic_testcase import assert_raises
+
+
+def exception_message(exception):
+    return exception.args[0]
+
+
+class AssertRaisesTest(TestCase):
+    # assert_raises is the basis of all testing. Therefore this test case must
+    # not use any other helper method.
+    
+    def _good_callable(self):
+        return lambda: None
+    
+    def test_fails_if_no_exception_was_raised(self):
+        try:
+            assert_raises(Exception, self._good_callable())
+        except AssertionError:
+            pass
+        else:
+            self.fail('No assertion raised')
+    
+    def _fail_with(self, exception):
+        def failing_callable():
+            raise exception
+        return failing_callable
+    
+    def test_passes_if_callable_raised_exception(self):
+        assert_raises(ValueError, self._fail_with(ValueError()))
+    
+    def test_returns_caught_exception_instance(self):
+        expected_exception = ValueError('foobar')
+        e = assert_raises(ValueError, self._fail_with(expected_exception))
+        assert expected_exception == e
+        assert id(expected_exception) == id(e)
+    
+    def test_callable_can_also_raise_assertion_error(self):
+        expected_exception = AssertionError('foobar')
+        e = assert_raises(AssertionError, self._fail_with(expected_exception))
+        assert expected_exception == e
+        assert id(expected_exception) == id(e)
+    
+    def test_passes_unexpected_exceptions(self):
+        try:
+            assert_raises(ValueError, self._fail_with(AssertionError()))
+        except AssertionError:
+            pass
+        else:
+            self.fail('Did not raise ValueError')
+    
+    def test_fails_with_default_error_message(self):
+        try:
+            assert_raises(ValueError, self._good_callable())
+        except AssertionError, e:
+            assert 'ValueError not raised!' == exception_message(e), repr(exception_message(e))
+        else:
+            self.fail('AssertionError not raised!')
+    
+    def test_can_specify_additional_custom_message(self):
+        try:
+            assert_raises(ValueError, self._good_callable(), message='Foo')
+        except AssertionError, e:
+            assert 'ValueError not raised! Foo' == exception_message(e), repr(exception_message(e))
+        else:
+            self.fail('AssertionError not raised!')
+
