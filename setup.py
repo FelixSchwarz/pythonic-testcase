0aa836502b02941011688e7925143b9ec91267c8
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2015-10-07 20:41:13 +0200

    back to dev mode

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(*rnames):
 
 setup(
     name = 'pythonic_testcase',
-    version = '1.0.2',
+    version = '1.0.3dev',
     description = 'standalone pythonic assertions',
     long_description=(read('Changelog.txt')),
 
